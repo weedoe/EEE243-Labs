706c17b7608b8da8f24072e962697e8fe2bb6f67
William Paterson <weedoe@weedoe.ca>
2023-11-24 23:42:41 -0500

    Assignment 9 start

---
+++ Assignments/a9/1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int checkPrime(int n);
+int main() {
+    int query;
+    scanf("%d", &query);
+    checkPrime(query);
+    return 0;
+}
+
+int checkPrime(int n) {
+   if(checkPrime(n) %)
+}
